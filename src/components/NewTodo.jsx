0bdb9f243174a818061c00c6b9c0ae082b363655
Stan <stantonmoore42@gmail.com>
2022-04-04 14:47:40 -0400

    Formatting

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -3,13 +3,13 @@ import { useNavigate } from 'react-router-dom'
 import { createTodo } from '../services/todos-api'
 
 export function NewTodo() {
-    let navigate = useNavigate()
-    const addNew = (e) => {
-        e.preventDefault()
-        let obj = {description: e.target.description.value, complete:false}
-        createTodo(obj)
-        navigate('/')
-    }
+  let navigate = useNavigate()
+  const addNew = (e) => {
+      e.preventDefault()
+      let obj = {description: e.target.description.value, complete:false}
+      createTodo(obj)
+      navigate('/')
+  }
   return (
     <div>
       <form onSubmit={addNew}>
